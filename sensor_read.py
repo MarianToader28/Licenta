c0041c2bcfa9b9db9d0b697e5c00a86bd734ba82
MarianToader2846 <toadermaryan@gmail.com>
2021-06-09 11:36:02 +0300

    June update

--- sensor_read.py
+++ sensor_read.py
@@ -1,37 +1,97 @@
 import Adafruit_DHT
 import time
 import datetime
+import RPi.GPIO as GPIO
 import sqlite3
 import twilio
 from twilio.rest import Client
 
 account_sid = 'ACf5491c0885b17c1d480e27b6d6976e1b'
-auth_token = '47e52d94dca7b7f749b213a4708e5bb1'
+
 client = Client(account_sid, auth_token)
 
-def getData():
+SPICLK = 11
+SPIMISO = 9
+SPIMOSI = 10
+SPICS = 8
+mq7_apin = 0
 
+def init_spi():
+         GPIO.setwarnings(False)
+         GPIO.cleanup()			#clean up at the end of your script
+         GPIO.setmode(GPIO.BCM)		#to specify whilch pin numbering system
+         # set up the SPI interface pins
+         GPIO.setup(SPIMOSI, GPIO.OUT)
+         GPIO.setup(SPIMISO, GPIO.IN)
+         GPIO.setup(SPICLK, GPIO.OUT)
+         GPIO.setup(SPICS, GPIO.OUT)
+
+#read SPI data from MCP3008 chip,8 possible adc's (0 thru 7)
+def readadc(adcnum, clockpin, mosipin, misopin, cspin):
+        if ((adcnum > 7) or (adcnum < 0)):
+                return -1
+        GPIO.output(cspin, True)	
+
+        GPIO.output(clockpin, False)  # start clock low
+        GPIO.output(cspin, False)     # bring CS low
+
+        commandout = adcnum
+        commandout |= 0x18  # start bit + single-ended bit
+        commandout <<= 3    # we only need to send 5 bits here
+        for i in range(5):
+                if (commandout & 0x80):
+                        GPIO.output(mosipin, True)
+                else:
+                        GPIO.output(mosipin, False)
+                commandout <<= 1
+                GPIO.output(clockpin, True)
+                GPIO.output(clockpin, False)
+
+        adcout = 0
+        # read in one empty bit, one null bit and 10 ADC bits
+        for i in range(12):
+                GPIO.output(clockpin, True)
+                GPIO.output(clockpin, False)
+                adcout <<= 1
+                if (GPIO.input(misopin)):
+                        adcout |= 0x1
+
+        GPIO.output(cspin, True)
+        
+        adcout >>= 1       # first bit is 'null' so drop it
+        return adcout
+
+
+def getData():
+    init_spi()
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setwarnings(False)  
+    GPIO.setup(12,GPIO.OUT)
+    GPIO.output(12,GPIO.HIGH)
     sensor = Adafruit_DHT.DHT22
     pin = 17
     reading_time = datetime.datetime.now().strftime("%Y/%m/%d, %H:%M:%S")
     humidity, temperature = Adafruit_DHT.read_retry(sensor, pin) # temperature, humidty read from sensor
-    if temperature is not None and humidity is not None: #it only print valid readings
-        print("{}: Temperature is: {:.2f} °C, Humidity is: {:.2f} ".format(reading_time,temperature, humidity))
-        if temperature >= 23.5:
+    COlevel = readadc(mq7_apin, SPICLK, SPIMOSI, SPIMISO, SPICS)
+    if temperature is not None and humidity is not None and COlevel is not None: #it only print valid reading  
+        print("{}: Temperature is: {:.2f} °C, Humidity is: {:.2f}, CO level is: {} ppm".format(reading_time,temperature, humidity,(COlevel/1024)*1000000/100000))
+        if temperature >= 27:
             client.messages.create(from_="+18022780315", body="Temperature is too high", to="+400753342380")
-        if humidity >= 65:
+        if humidity >= 70:
            client.messages.create(from_="+18022780315", body="Humidity is too high", to="+400753342380")
+        #if COlevel >= 10:
+         #  client.messages.create(from_="+18022780315", body="CO level is too high", to="+400753342380")
            
 
-        return reading_time, temperature, humidity
+        return reading_time, temperature, humidity, COlevel
     
 
-def putDataInDB(reading_time, temperature, humidity):   
+def putDataInDB(reading_time, temperature, humidity, COlevel):   
 
     conn = sqlite3.connect('Measurements.db')
     crs = conn.cursor()
-    crs.execute("INSERT INTO data VALUES (?,?,?)",
-                 ('{:}'.format(reading_time),'{:.2f}'.format(temperature),' {:.2f}'.format(humidity)))
+    crs.execute("INSERT INTO data VALUES (?,?,?,?)",
+                 ('{:}'.format(reading_time),'{:.2f}'.format(temperature),' {:.2f}'.format(humidity),'{}'.format((COlevel/1024)*1000000/100000)))
     conn.commit()
     conn.close()
 
@@ -39,19 +99,28 @@ def putDataInDB(reading_time, temperature, humidity):
 def showData():
 
     run = True
-
+   
     try:
 
         while run:
-            read_time, temp, hum = getData()
-            putDataInDB(read_time, temp, hum)
-            time.sleep(15)
+            #GPIO.setup(12,GPIO.OUT)
+            #GPIO.output(12,GPIO.HIGH)
+            read_time, temp, hum, CO = getData()
+            putDataInDB(read_time, temp, hum, CO)
+            time.sleep(5)
 
     except KeyboardInterrupt: #press ctl+c to stop the infinite loop
 
         print (' Program stopped')
 
-        run = False #when we want to stop to receive data   
+        run = False #when we want to stop to receive data  
+
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setwarnings(False)
+        GPIO.setup(12,GPIO.OUT)
+        GPIO.output(12,GPIO.LOW)
+
+        GPIO.cleanup()
 
 showData()
         
--- web.py
+++ web.py
@@ -1,5 +1,5 @@
 import sqlite3
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify, request, flash, redirect
 
 
 app = Flask(__name__)
@@ -12,43 +12,50 @@ def getLastData():
         read_time = str(data[0])
         temp = data[1]
         hum = data[2]
+        CO = data[3]
        
-    return read_time,temp, hum
+    return read_time,temp, hum, CO
 
 @app.route('/')
 def index():
-	read_time,temp, hum = getLastData()
+	read_time,temp, hum, CO = getLastData()
 	templateData = {
         'read_time': read_time,
         'temp'  : temp,
-        'hum'	: hum
-      	
-	}
+        'hum'	: hum,
+        'CO'    : CO
+    }
     
 	return render_template('index.html', **templateData)
 
 @app.route("/graph")
 def getRecentMeasurements():
-    crs.execute("SELECT date_time, temperature FROM data ORDER BY date_time DESC LIMIT 30")
+    crs.execute("SELECT date_time, temperature FROM data ORDER BY date_time DESC LIMIT 60")
     data = crs.fetchall()
     labels = [row[0] for row in reversed(data)]
     values = [row[1] for row in reversed(data)]
-    crs.execute('SELECT date_time, humidity FROM data ORDER BY date_time DESC LIMIT 30')
+    crs.execute('SELECT date_time, humidity FROM data ORDER BY date_time DESC LIMIT 60')
     data1 = crs.fetchall()
     labels_hum = [row[0] for row in reversed(data1)]
     values_hum = [row[1] for row in reversed(data1)]
+    crs.execute('SELECT date_time, CO FROM data ORDER BY date_time DESC LIMIT 60')
+    data2 = crs.fetchall()
+    labels_CO = [row[0] for row in reversed(data2)]
+    values_CO = [row[1] for row in reversed(data2)]
 
-    return render_template("Graphs.html",labels=labels, values=values,labels_hum=labels_hum, values_hum=values_hum)
+    return render_template("Graphs.html",labels=labels, values=values,labels_hum=labels_hum, values_hum=values_hum, labels_CO=labels_CO, values_CO=values_CO)
 
-#@app.route('/hum')
-#def getRecentHums():
- #   crs.execute('SELECT date_time, humidity FROM data ORDER BY date_time DESC LIMIT 30')
-  #  data1 = crs.fetchall()
-   # labels_hum = [row[0] for row in reversed(data1)]
-    #values_hum = [row[1] for row in reversed(data1)]
 
-    #return render_template("humidity.html", labels_hum=labels_hum, values_hum=values_hum)
-    
+@app.route("/range",methods=["POST","GET"])
+def range():
+    if request.method == "POST":
+        From = request.form['From']
+        print(From)
+        crs.execute("SELECT date_time, temperature from data WHERE date_time = '{}'".format(From))
+        data3 = crs.fetchall()
+        new_date = [row[0] for row in reversed(data3)]
+        new_temp = [row[1] for row in reversed(data3)]
+    return jsonify({'htmlresponse': render_template('response.html',new_date=new_date,new_temp=new_temp)})
 
 #if __name__ == '__main__':
  #  app.run(debug=True,host = '192.168.0.104')
